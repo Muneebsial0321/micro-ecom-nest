7c41a3c292cd3c4b4fbc6fd1ce63210db85c891a
Muneeb Ur Rehman <137985020+Muneebsial0321@users.noreply.github.com>
2025-02-07 09:13:57 +0500

    bug fixed

---
+++ deploy.bat
@@ -0,0 +1,8 @@
+echo "deploying------------->"
+echo "ADDING------------->"
+git add .
+echo "COMMITING------------->"
+git commit -m "bug fixed"
+git push origin main
+echo "deployed------------->"
+
