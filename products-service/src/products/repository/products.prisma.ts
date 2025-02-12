ad3f0b360aa0ba481d53b5ced45bc12d8d0aeb3e
Muneeb Ur Rehman <137985020+Muneebsial0321@users.noreply.github.com>
2025-02-12 08:25:21 +0500

    bug fixed

--- products-service/src/products/repository/products.prisma.ts
+++ products-service/src/products/repository/products.prisma.ts
@@ -1,4 +1,14 @@
 import { Injectable } from "@nestjs/common";
+import { DbService } from "src/db/db.service";
 
 @Injectable()
-export class ProductRepository{}
+export class ProductRepository{
+
+    constructor(private readonly db:DbService ){}
+
+    create(){}
+    getAll(){}
+    getOne(){}
+    updateOne(){}
+    deleeOne(){}
+}
