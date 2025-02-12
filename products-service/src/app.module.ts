f839c229a9da56eb210bb9e9bdbd024bb097b0ce
Muneeb Ur Rehman <137985020+Muneebsial0321@users.noreply.github.com>
2025-02-12 08:21:55 +0500

    bug fixed

--- products-service/src/app.module.ts
+++ products-service/src/app.module.ts
@@ -3,10 +3,11 @@ import { ProductsModule } from './products/products.module';
 import { ViewsModule } from './views/views.module';
 import { ReviewsModule } from './reviews/reviews.module';
 import { PurchasedModule } from './purchased/purchased.module';
+import { DbModule } from './db/db.module';
 
 
 @Module({
-  imports: [ProductsModule, ViewsModule, ReviewsModule, PurchasedModule],
+  imports: [ProductsModule, ViewsModule, ReviewsModule, PurchasedModule, DbModule],
 
 })
 export class AppModule {}
--- products-service/src/products/products.module.ts
+++ products-service/src/products/products.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { ProductsService } from './products.service';
 import { ProductsController } from './products.controller';
+import { ProductElasticSearch } from './repository/products.elasticsearch';
+import { ProductRepository } from './repository/products.prisma';
 
 @Module({
   controllers: [ProductsController],
-  providers: [ProductsService],
+  providers: [ProductsService,ProductElasticSearch,ProductRepository],
 })
 export class ProductsModule {}
